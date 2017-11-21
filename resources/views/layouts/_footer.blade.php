1b8aeb2697e1de11f7dc1f05649f4e4fb7a9b718
Bob <374123140@qq.com>
2017-11-21 08:55:26 +0000

    Finish static-pages

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+	<div class="container">
+		<p class="pull-left">
+			 由Bob设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+		</p>
+
+		<p class="pull-right">联系我们</p>
+	</div>
+
+</footer>
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('title','首页')
+
+@section('content')
+<div>
+	<h1>这是首页</h1>
+</div>
+@stop
