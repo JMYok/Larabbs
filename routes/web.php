d302402bc96e1a566b0a86b89ee02908cb688166
Bob <374123140@qq.com>
2017-11-22 08:55:58 +0000

    User profile page

--- routes/web.php
+++ routes/web.php
@@ -13,19 +13,21 @@
 
 Route::get('/','PagesController@root')->name('root');
 
-// Authentication Routes...
+// 登录认证路由
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
-// Registration Routes...
+// 注册路由
 Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
 Route::post('register', 'Auth\RegisterController@register');
 
-// Password Reset Routes...
+//密码重设路由
 Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
+//用户页面路由(显示个人信息，编辑个人资料，处理编辑)
+Route::resource('users','UserController',['only'=>['show','update','edit']]);
 
